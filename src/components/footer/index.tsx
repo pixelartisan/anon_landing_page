b61263f83f917a850b22e87f49de75aa3027cf4e
Razvan Baba <babarazvan@yahoo.com>
2019-09-28 15:37:55 +0300

    UPDATE COMPANY DETAILS FOOTER [RARESJ] + ERRORS

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -14,6 +14,7 @@ import Preamble from '../generic/preamble';
 import FooterColumn from './footerColumn';
 import FooterContact from './footerContact';
 import ChatLink from '../generic/chatLink';
+//ADDED FOOTERCONTACT
 
 // Actions
 import { setScrollElement } from '../../actions';
