cecde3bab54b599a5d6312cd97a0cf000a8b46e6
Rares Josanu <raresjosanu@yahoo.com>
2019-09-30 15:48:08 +0300

    optimizari mobile

--- src/components/pricing/pricingFeatures.tsx
+++ src/components/pricing/pricingFeatures.tsx
@@ -12,31 +12,31 @@ const PricingFeatures = () => (
 		<Col lg={{ size: 11 }}>
 			<Row className="row d-flex justify-content-center">
 				<Col lg={{ size: 'auto' }} md={{ size: 4 }} sm={{ size: 6 }} className="mt-4 mt-md-0">
-					<div className="pricing-feature d-flex align-items-center justify-content-center">
+					<div className="pricing-feature d-flex align-items-center">
 						<FontAwesomeIcon className="icon" icon={faCircle} />
 						<p>Fully anonymous</p>
 					</div>
 				</Col>
 				<Col lg={{ size: 'auto' }} md={{ size: 4 }} sm={{ size: 6 }} className="mt-4 mt-md-0">
-					<div className="pricing-feature d-flex align-items-center justify-content-center">
+					<div className="pricing-feature d-flex align-items-center">
 						<FontAwesomeIcon className="icon" icon={faCircle} />
 						<p>24/7 Heroic support</p>
 					</div>
 				</Col>
 				<Col lg={{ size: 'auto' }} md={{ size: 4 }} sm={{ size: 6 }} className="mt-4 mt-md-0">
-					<div className="pricing-feature d-flex align-items-center justify-content-center">
+					<div className="pricing-feature d-flex align-items-center">
 						<FontAwesomeIcon className="icon" icon={faCircle} />
 						<p>Multiple datacenters</p>
 					</div>
 				</Col>
 				<Col lg={{ size: 'auto' }} md={{ size: 4 }} sm={{ size: 6 }} className="mt-4 mt-lg-0">
-					<div className="pricing-feature d-flex align-items-center justify-content-center">
+					<div className="pricing-feature d-flex align-items-center">
 						<FontAwesomeIcon className="icon" icon={faCircle} />
 						<p>Affordable pricing</p>
 					</div>
 				</Col>
 				<Col lg={{ size: 'auto' }} md={{ size: 4 }} sm={{ size: 6 }} className="mt-4 mt-lg-0">
-					<div className="pricing-feature d-flex align-items-center justify-content-center">
+					<div className="pricing-feature d-flex align-items-center">
 						<FontAwesomeIcon className="icon" icon={faCircle} />
 						<p>Instant activation</p>
 					</div>
