4a02ceb90be531090de70b2f66aa45cc5d7e1d8c
Razvan Baba <babarazvan@yahoo.com>
2020-01-16 17:59:57 +0200

    add locations page and add _headers

--- src/components/locationscomponent/index.tsx
+++ src/components/locationscomponent/index.tsx
@@ -1,42 +1,16 @@
 import React, { Component } from 'react';
 import requestHandler from '../../utils/request';
-import {DEFAULT_TAG, LOCATIONS_URL, PRICES_URL} from '../../constants';
+import {LOCATIONS_URL} from '../../constants';
 
-interface Props {
-	doSetPrices(data: object): void,
-	discounts?: object,
-	prices?: object,
-	billingCycles: Array<object>
-}
+import data from "../../locations.json";
+
+const contintentsList = data.SocialMedias;
 
 
 
 class Contintents extends Component {
-	constructor(props: Props) {
-		super(props);
-
-		this.state = {
-			selectedBillingCycle: 1,
-			selectedProduct: 'proxy',
-			selectedProductType: 'dedicated',
-			selectedQuantity: 10,
-			selectedTag: DEFAULT_TAG
-		};
-	}
 
 	render() {
-		const { contintentsList } = this.props;
-
-		requestHandler(LOCATIONS_URL)
-			.then(({ data, error }) => {
-				if (error) {
-					console.error(error);
-					return;
-				}
-
-				contintentsList(data);
-			})
-			.catch(console.error);
 		return (
 			<ul>
 				{contintentsList.map(s => (<li>{s}</li>))}
