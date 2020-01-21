aacddbbba5a650d247a17b921f53fef241b805ad
Razvan Baba <babarazvan@yahoo.com>
2020-01-21 10:38:02 +0200

    generate product pages

--- src/pages/location.tsx
+++ src/pages/location.tsx
@@ -1,85 +1,87 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import {connect} from "react-redux";
 
-
-interface State {
-    selectedProduct: string,
-    selectedProductType: string,
-    selectedTag: {
-        value: string,
-        label: string
-    }
-}
-
-
-// Components
+// Constants
+import NavBar from '../components/navbar';
+import MenuItem from '../components/navbar/secondaryMenuItem';
 import Footer from '../components/footer';
 import Extra from '../components/extra';
 import ProductsHero from '../components/hero/productsHero';
-
-// Scss
+import Pricing from '../components/pricing';
+import Features from '../components/features';
+import Reviews from '../components/reviews';
 import '../styles/components/products.scss';
-
 // Constants
 import {
     SEO_DESCRIPTION,
     SEO_KEYWORDS
 } from '../settings';
-import {ILocationsResponse, ICitiesResponse, ICountries} from '../interfaces';
-import {DEFAULT_TAG, LOCATION_ROUTE, LOCATIONS_URL} from "../constants";
-import requestHandler from "../utils/request";
 import LocationText from "../components/locationtext";
+import {DEFAULT_TAG} from "../constants";
 
-
-const search = document.location.search; // could be '?foo=bar'
-const params = new URLSearchParams(search);
-const locationName = params.get('location'); // location
-const productName = params.get('product'); // location
-
-
-var productNameHuman = 'Proxy';
-switch (productName) {
-    case 'shared_proxy':
-        productNameHuman = 'shared proxy';
-        break;
-    case 'shared_socks':
-        productNameHuman = 'shared SOCKS5';
-        break;
-    case 'socks':
-        productNameHuman = 'dedicated SOCKS5';
-        break;
-    case 'proxy':
-        productNameHuman = 'dedicated proxy';
-        break;
+interface Props {
 
 }
-const pageTitle = locationName + ' ' + productNameHuman;
-const btnTitle = 'Buy ' + locationName + ' ' + productNameHuman;
+
+interface State {
+
+}
+
+class LocationPage extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+
+        const search = this.props.location.search; // could be '?foo=bar'
+        const params = new URLSearchParams(search);
+        const locationName = params.get('location'); // location
+        const productName = params.get('product'); // location
+
+
+        this.productNameHuman = 'Proxy';
+        switch (productName) {
+            case 'shared_proxy':
+                this.productNameHuman = 'shared proxy';
+                break;
+            case 'shared_socks':
+                this.productNameHuman = 'shared SOCKS5';
+                break;
+            case 'socks':
+                this.productNameHuman = 'dedicated SOCKS5';
+                break;
+            case 'proxy':
+                this.productNameHuman = 'dedicated proxy';
+                break;
+
+        }
+        this.pageTitle = locationName + ' ' + this.productNameHuman;
+        this.btnTitle = 'Buy ' + locationName + ' ' + this.productNameHuman;
+        console.log(this);
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <Helmet>
+                    <title>Http proxies</title>
+                    <meta name="description" content={SEO_DESCRIPTION}/>
+                    <meta name="keywords" content={SEO_KEYWORDS}/>
+                </Helmet>
+
+                <ProductsHero title={this.pageTitle} btnText={this.btnTitle}>
+                    Buy {this.productNameHuman} from {this.locationName} !
+                    If you're looking to purchase {this.productNameHuman} from {this.locationName} look no further because we have them
+                    available in our
+                    stock. {this.locationName} {this.productName} locations include <LocationText/>
+                </ProductsHero>
 
 
 
-const LocationPage = () => (
-    <React.Fragment>
-        <Helmet>
-            <title>Http proxies</title>
-            <meta name="description" content={SEO_DESCRIPTION}/>
-            <meta name="keywords" content={SEO_KEYWORDS}/>
-        </Helmet>
+                <Extra/>
 
-        <ProductsHero title={pageTitle} btnText={btnTitle}>
-            Buy {productNameHuman} from {locationName} !
-            If you're looking to purchase {productNameHuman} from {locationName} look no further because we have them
-            available in our
-            stock. {locationName} {productName} locations include <LocationText/>
-        </ProductsHero>
-
-
-
-        <Extra/>
-
-        <Footer/>
-    </React.Fragment>
-);
+                <Footer/>
+            </React.Fragment>
+        );
+    }
+}
 
 export default LocationPage;
